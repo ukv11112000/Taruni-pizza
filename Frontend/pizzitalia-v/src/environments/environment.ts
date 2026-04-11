a2254fba0b45366791de7fe84ccb3fb3855ae116
ukv11112000 <84135811+ukv11112000@users.noreply.github.com>
2026-04-11 10:04:56 -0500

    Refactor environment.ts by removing comments

--- Frontend/pizzitalia-v/src/environments/environment.ts
+++ Frontend/pizzitalia-v/src/environments/environment.ts
@@ -1,23 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
+  apiBaseUrl: 'http://localhost:8080',
   loginUrl: 'http://localhost:8080/',
-  customerUrl:"http://localhost:8080/customer/",
-  userUrl:"http://localhost:8080/user/",
-  coupanUrl:"http://localhost:8080/coupan/",
-  pizzaOrderUrl:"http://localhost:8080/pizzaOrder/",
-  orderUrl:"http://localhost:8080/order/",
-  pizzaUrl:"http://localhost:8080/pizza/"
+  customerUrl: 'http://localhost:8080/customer/',
+  userUrl: 'http://localhost:8080/user/',
+  coupanUrl: 'http://localhost:8080/coupan/',
+  pizzaOrderUrl: 'http://localhost:8080/pizzaOrder/',
+  orderUrl: 'http://localhost:8080/order/',
+  pizzaUrl: 'http://localhost:8080/pizza/'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
